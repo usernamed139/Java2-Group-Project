339351094cea7433434400266972baebf6b69ea4
username <username139p580248r@gmail.com>
2026-05-21 16:18:25 -0700

    Create Tile.java

---
+++ Tile.java
@@ -0,0 +1,16 @@
+public class Tile {
+  public int x = 0;
+  public int y = 0;
+  
+  String toString() {
+    return " ";
+  }
+
+  Color toColor() {
+    return Color.WHITE;
+  }
+
+  void tick() {
+    
+  }
+}
