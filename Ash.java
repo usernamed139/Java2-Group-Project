639719a9f4eff15508ba7380d5be1166e337e021
jerryisthisname123 <108350062+jerryisthisname123@users.noreply.github.com>
2026-05-22 12:12:25 -0700

    Add files via upload

---
+++ Ash.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+
+public class Ash extends Tile{
+    @Override
+    public String toString(){
+        return ".";
+    }
+    @Override
+    public Color toColor(){
+        return Color.GRAY;
+    }
+}
